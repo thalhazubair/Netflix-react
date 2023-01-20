7b498e76eb72fd3ad919c9582c5a947c8397c855
thalhazubair <thalhaz999@gmail.com>
2023-01-20 18:00:11 +0530

    netflix with react

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import {API_KEY, imageUrl} from '../../constants/constants'
+import axios from '../../axios'
+import './Banner.css'
+
+function Banner() {
+    const [movie, setMovie] = useState([])
+    useEffect(() => {
+    axios.get(`trending/all/day?api_key=${API_KEY}`).then((response)=>{
+       
+        setMovie(response.data.results[ Math.floor(Math.random() * (10 - 1 + 1) +1)]);
+        
+    })
+    }, [])
+    
+  return (
+    <div 
+    style={{backgroundImage:`url(${movie ? imageUrl+movie.backdrop_path : ""})`}}
+    className='banner'>
+<div className='content'>
+    <h1 className='title'>{movie ? movie.name : ""}</h1>
+    <div className='banner_buttons'>
+        <button className='button'>Play</button>
+        <button className='button'>My List</button>
+    </div>
+    <h1 className='description'>{movie ? movie.overview: ""}</h1>
+</div>
+<div className='fade_bottom'></div>
+    </div>
+  )
+}
+
+export default Banner
